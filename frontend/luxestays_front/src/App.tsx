c4e68dac5dc4208fb867701bbca54fd035ebf96d
Anghelinna Alva <coartac.anghelina.encinas@gmail.com>
2025-06-18 11:50:49 -0500

    Adicionado el header

--- frontend/luxestays_front/src/App.tsx
+++ frontend/luxestays_front/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="min-h-screen">
-      {/* Aquí va el Header */}
-      <main>{/* Aquí van las demás páginas */}</main>
+      <Header/>
+      <main> {/* Aquí van las demás páginas */} </main>
       <Footer />
     </div>
   );
