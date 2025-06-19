f9b1912141addeb7094f0ce7676dd9a891045b72
Anghelinna Alva <coartac.anghelina.encinas@gmail.com>
2025-06-18 19:25:21 -0500

    Agregada la sección de FAQ

--- frontend/luxestays_front/src/App.tsx
+++ frontend/luxestays_front/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero"; 
 import WhyChooseUs from "./components/WhyChooseUs";
 import Services from "./components/Services";
+import FAQ from "./components/FAQ";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <WhyChooseUs />
         <Services />
+        <FAQ />
       </main>
       <Footer />
     </div>
