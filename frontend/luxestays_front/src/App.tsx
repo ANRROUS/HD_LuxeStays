1a5a8594f6a175e2ee33e4dabf4ee4e770a5323a
Anghelinna Alva <coartac.anghelina.encinas@gmail.com>
2025-06-18 19:12:20 -0500

    Agregado de sección Beneficios

--- frontend/luxestays_front/src/App.tsx
+++ frontend/luxestays_front/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero"; 
+import WhyChooseUs from "./components/WhyChooseUs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <WhyChooseUs />
       </main>
       <Footer />
     </div>
